0fdc5ba374c6cc9931b90ff3caae53054fba55ea
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-01-08 11:06:34 +0100

    first function

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,11 @@
 // CODE According to specification
 function click_filter_element (event) {
 
+  const filter_dom = event.currentTarget;
+  filter_dom.classList.toggle("selected");
+
+  update_programmes();
+
   /*
     ARGUMENTS
       event: event-object created when user clicks on one of the filter elements.
